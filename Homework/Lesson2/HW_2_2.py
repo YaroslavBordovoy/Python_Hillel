cea9eb5b63d501e16133ebc28c8a8603ce4829e9
Yaroslav Bordovoy <superbobs777@gmail.com>
2022-05-27 17:27:51 +0300

    Add fix

--- Homework/Lesson2/HW_2_2.py
+++ Homework/Lesson2/HW_2_2.py
@@ -2,10 +2,12 @@
 
 a = int(input('Введите трехзначное положительное целое число: '))
 
-number_1 = a % 10
-number_2 = (a // 10) % 10
-number_3 = (a // 100 ) % 10
+number_1 = a // 100
+number_2 = a % 100 // 10
+number_3 = a % 10
 
-result = str(number_1) + str(number_2) + str(number_3)
+print('Результат: ', number_3 * 100 + number_2 * 10 + number_1)
 
-print('Результат:', result)
+
+
+
