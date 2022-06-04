697fb2ee496c750ca73e5487b5df9ee31a52b19d
Yaroslav Bordovoy <superbobs777@gmail.com>
2022-06-04 22:54:51 +0300

    Add 8 task

---
+++ Homework/Lesson4/HW_4_3.py
@@ -0,0 +1,42 @@
+# Срезы строки
+
+string = input('Enter a string: ')
+
+# Все символы с четным индексом в строке (индексация начинается с 0)
+
+print('Even indexes: ', string[::2])
+
+# Все символы с нечетным индексом
+
+print('Odd indexes: ', string[1::2])
+
+# Пятый символ в строке, если он есть (если строка не слишком короткая)
+
+print('Fifth character: ', string[5])
+
+# Все до десятого символа
+
+print('All up to the tenth character: ', string[:10])
+
+# Последние 5 символов строки
+
+print('Last five characters: ', string[-5:])
+
+# Строку в обратном порядке
+
+print('Reverse order: ', string[::-1])
+
+# Строку в обратном порядке через один символ (с увеличенным шагом)
+
+print('Reverse order through one character: ', string[::-2])
+
+# Длина строки
+
+print('String length: ', len(string))
+
+# Палиндром
+
+if string == string[::-1]:
+    print('Is a palindrome')
+else:
+    print('Is not a palindrome')
